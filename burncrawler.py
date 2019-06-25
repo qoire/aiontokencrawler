56a79c311e9bce52c0d6241b84748305b773ce1c
qoire <mr.sun.yao@gmail.com>
2019-06-24 23:28:50 -0400

    update crawlers

--- burncrawler.py
+++ burncrawler.py
@@ -13,7 +13,7 @@ FINISH = 7854631
 BALANCE_DIR = 'output'
 GETH_DB_PATH = "/home/yao/Ethereum/geth-linux-amd64-1.8.27-4bcc0a37/geth-storage/geth/chaindata"
 
-OUT_DB_PATH = "/media/yao/STORAGE/aion-erc20/erc20-history.sqlite"
+OUT_DB_PATH = "/media/yao/STORAGE/aion-erc20/erc20-burnhistory.sqlite"
 
 TRANSFER_EVENT = b"Transfer(address,address,uint256)"
 TRANSFER_EVENT_HASH = keccak256(TRANSFER_EVENT)
@@ -35,21 +35,14 @@ def execute_transfer(state, _from, to, amount, type, txhash):
     to_balance = 0
 
     if not _from == to:
-        if _from in state:
-            from_balance = state[_from]
-
         if to in state:
             to_balance = state[to]
-
         state[to] = to_balance + amount
-        if not (type == "mint"):
-            state[_from] = from_balance - amount
 
-    # # check invariant should always be true
-    # print("(%s) %s[%s] -> %s -> %s[%s] type: %s" %
-    #     (txhash.hex(), _from.hex(), from_balance, amount, to.hex(), to_balance, type))
-    if _from in state:
-        assert state[_from] >= 0
+        # check invariant should always be true
+        print("(%s) %s[%s] -> %s -> %s[%s] type: %s" %
+            (txhash.hex(), _from.hex(), from_balance, amount, to.hex(), to_balance, type))
+
     return (state, (txhash, _from, from_balance, to, to_balance, amount, type))
 
 def loop(database, i, state):
@@ -75,31 +68,13 @@ def loop(database, i, state):
                 continue
 
             if not is_mint:
-                if l.topics[0] == TRANSFER_EVENT_HASH:
-                    f = format_input(l)
-                    (state, transfer) = execute_transfer(state, f[0], f[1], f[2], "transfer", rec.txhash)
-                    transfers.append(transfer)
-                    state_changed = True
-                    continue
-
                 if l.topics[0] == BURN_EVENT_HASH:
                     f = format_input(l)
-                    (state, transfer) = execute_transfer(state, f[0], ZERO_ADDR, f[2], "burn", rec.txhash)
+                    (state, transfer) = execute_transfer(state, f[0], f[1], f[2], "burn", rec.txhash)
                     transfers.append(transfer)
                     state_changed = True
                     continue
 
-            else:
-                # heuristic: the mint function only ever calls the contract
-                # we assume that if the transaction directly calls the contract
-                # and something is transferred (on the logs), then it must
-                # be a mint... (any counterexamples to this?)
-                if (l.topics[0] == TRANSFER_EVENT_HASH):
-                    f = format_input(l)
-                    (state, transfer) = execute_transfer(state, f[0], f[1], f[2], "mint", rec.txhash)
-                    transfers.append(transfer)
-                    state_changed = True
-                    continue
     return (state, state_changed, transfers)
 
 conn = sqlite3.connect(OUT_DB_PATH)
@@ -139,13 +114,6 @@ def commit_state(state, transfers, block_num):
         raise
     finally:
         c.close()
-
-def flushfile(state, block_num):
-    with open("/media/yao/MOVIES/aion-erc20/%s.csv" % block_num, 'w') as f:
-        w = csv.writer(f)
-        for k, v in state.items():
-            if (v != 0):
-                w.writerow([k.hex(), v])
 
 if __name__ == "__main__":
     try:
